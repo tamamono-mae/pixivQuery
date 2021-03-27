369af7d8a807b2635653c6752601e36f9902b8dc
tamamo <tamamo@tamamono.moe>
2021-03-27 20:34:40 +0800

    Finish Bot

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const config = require("../token/config.json");
+const config = require("../token/config2.json");
 const q = require("./query.js");
 const client = new Discord.Client();
 let patt = {
@@ -20,12 +20,11 @@ client.login(config.BOT_TOKEN);
 client.on("message", function(message) {
   var is_dm = message.channel.type == 'dm';
   if (message.author.bot) return;
-  if (!message.content.startsWith(config.prefix) && !is_dm) return;
-  var msgbody = (is_dm) ? message.content : message.content.slice(config.prefix.length);
-  if (urlDump(msgbody) != null)
-    q.pixivQuery(urlDump(msgbody)['data']).then(result => {
-      message.channel.send(q.query2msg(result,urlDump(msgbody)['website']))
+  //if (!message.content.startsWith(config.prefix) && !is_dm) return;
+  //var msgbody = (is_dm) ? message.content : message.content.slice(config.prefix.length);
+  if (urlDump(message.content) != null)
+    q.pixivQuery(urlDump(message.content)['data']).then(result => {
+      message.channel.send(q.query2msg(result,urlDump(message.content)['website']))
     });
-  //q.saucenao(msgbody).then(result => {message.channel.send("SauceNAO\n"+result)});
 
 });
