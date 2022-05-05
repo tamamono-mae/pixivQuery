aab6967d442b050b38687246295097e393d66e23
tamamo-s <79314960+tamamo-s@users.noreply.github.com>
2022-05-05 17:11:48 +0800

    Update node-fetch import method.

--- webRequest.js
+++ webRequest.js
@@ -1,4 +1,4 @@
-const fetch = require("node-fetch");
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 const cheerio = require("cheerio");
 const formData = require("form-data");
 const { htmlToText } = require('html-to-text');
